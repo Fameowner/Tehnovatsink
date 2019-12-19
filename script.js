3adfd29f1da85adb23b7352fdfabefa24578cd85
Fameowner <pavlikov-v@ya.ru>
2019-12-19 19:57:39 +0300

    mobile

--- script.js
+++ script.js
@@ -12,7 +12,60 @@ function currSlideController(n){
     }
 }
 
+function addSliderButtons() {
+        for (let i = 0; i < sliderItems.length; i++) {
+            let div = document.createElement('div');
+            if (i==0){
+                div.className = `slider-num-button current`;
+            } else {
+                div.className = `slider-num-button`;
+            }
+            div.id = `slider-num-button__`+i;
+            document.getElementById(`slider-num-buttons__01`).append(div);
+        }
+}
+
+function sliderNumButtonsActiveControll(c){
+    for (let j = 0; j < sliderNumButtons.length; j++) {
+        document.getElementById(`slider-num-button__`+j).classList.remove(`current`);
+    }
+    document.getElementById(`slider-num-button__`+c).classList.add(`current`);
+}
+
+addSliderButtons();
 currSlideController(currSlide);
+
+let sliderNumButtons = document.getElementsByClassName(`slider-num-button`);
+
+document.getElementById(`slider-num-buttons__01`).onclick = ()=>{
+    let target = event.target;
+    for (let i = 0; i < sliderItems.length; i++) {
+        if (target.id == `slider-num-button__`+i) {
+            currSlide = i;
+            sliderNumButtonsActiveControll(currSlide);
+            slider.scrollTo({
+                left: sliderItems[currSlide].getBoundingClientRect().left - sliderItems[0].getBoundingClientRect().left+1,
+                behavior: "smooth",
+                });
+            currSlideController(currSlide);
+        }
+    }
+}
+
+var sliderTouch=0;
+
+document.getElementById(`slider-item`).addEventListener(`touchstart`,(e)=>{
+    sliderTouch = e.touches[0].clientX;
+})
+
+document.getElementById(`slider-item`).addEventListener(`touchend`,(e)=>{
+    if (e.changedTouches[0].clientX > sliderTouch){
+        prev();
+    } else if(e.changedTouches[length].clientX < sliderTouch){
+        next();
+    }
+    return false
+})
 
 function next()
 {
@@ -23,6 +76,7 @@ function next()
                 behavior: "smooth"
                 });
             currSlideController(currSlide);
+            sliderNumButtonsActiveControll(currSlide);
         }
 }
 
@@ -35,6 +89,7 @@ function prev()
                 behavior: "smooth"
                 });
             currSlideController(currSlide);
+            sliderNumButtonsActiveControll(currSlide);
         }
 }
 
@@ -43,3 +98,5 @@ window.addEventListener("resize",()=>{
         left: sliderItems[currSlide].getBoundingClientRect().left - sliderItems[0].getBoundingClientRect().left+1
         });
 })
+
+
